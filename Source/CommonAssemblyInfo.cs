e869ee12d94d88b093456281685953432e7143a8
Michael Tsai <huanlin.tsai@gmail.com>
2018-03-31 17:03:50 +0800

    fixed #4

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.8.0.*")]
-[assembly: AssemblyFileVersion("2.8.0")]
+[assembly: AssemblyVersion("2.8.1.*")]
+[assembly: AssemblyFileVersion("2.8.1")]
