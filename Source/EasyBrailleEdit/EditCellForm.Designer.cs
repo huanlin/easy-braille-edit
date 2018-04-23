a94a6891a8c5dad538af4cee20955ce428ab79a7
Michael Tsai <huanlin.tsai@gmail.com>
2018-04-23 13:39:22 +0800

    fixed 19 btx 視窗在挑選點字時，有一個符號是空的。 [北市視協]

--- Source/EasyBrailleEdit/EditCellForm.Designer.cs
+++ Source/EasyBrailleEdit/EditCellForm.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.Location = new System.Drawing.Point(33, 41);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.Size = new System.Drawing.Size(66, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "明眼字";
             // 
@@ -58,7 +58,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(48, 128);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.Size = new System.Drawing.Size(47, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "點字";
             // 
@@ -68,41 +68,41 @@
             this.label3.Location = new System.Drawing.Point(33, 80);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.Size = new System.Drawing.Size(66, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "注音碼";
             // 
             // txtChar
             // 
-            this.txtChar.Location = new System.Drawing.Point(92, 38);
+            this.txtChar.Location = new System.Drawing.Point(106, 38);
             this.txtChar.MaxLength = 1;
             this.txtChar.Name = "txtChar";
-            this.txtChar.Size = new System.Drawing.Size(70, 25);
+            this.txtChar.Size = new System.Drawing.Size(70, 29);
             this.txtChar.TabIndex = 3;
             this.toolTip1.SetToolTip(this.txtChar, "只能輸入一個字元，若輸入空白字元則為空方。");
-            this.txtChar.Validating += new System.ComponentModel.CancelEventHandler(this.txtChar_Validating);
             this.txtChar.TextChanged += new System.EventHandler(this.txtChar_TextChanged);
+            this.txtChar.Validating += new System.ComponentModel.CancelEventHandler(this.txtChar_Validating);
             // 
             // cboPhCode
             // 
             this.cboPhCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboPhCode.FormattingEnabled = true;
-            this.cboPhCode.Location = new System.Drawing.Point(92, 77);
+            this.cboPhCode.Location = new System.Drawing.Point(106, 77);
             this.cboPhCode.Name = "cboPhCode";
-            this.cboPhCode.Size = new System.Drawing.Size(133, 23);
+            this.cboPhCode.Size = new System.Drawing.Size(133, 26);
             this.cboPhCode.TabIndex = 4;
             this.cboPhCode.SelectedIndexChanged += new System.EventHandler(this.cboPhCode_SelectedIndexChanged);
             // 
             // txtBraille
             // 
             this.txtBraille.Font = new System.Drawing.Font("SimBraille", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBraille.Location = new System.Drawing.Point(92, 114);
+            this.txtBraille.Location = new System.Drawing.Point(101, 116);
             this.txtBraille.MaxLength = 3;
             this.txtBraille.Name = "txtBraille";
-            this.txtBraille.Size = new System.Drawing.Size(133, 39);
+            this.txtBraille.Size = new System.Drawing.Size(133, 47);
             this.txtBraille.TabIndex = 5;
-            this.txtBraille.Validating += new System.ComponentModel.CancelEventHandler(this.txtBraille_Validating);
             this.txtBraille.TextChanged += new System.EventHandler(this.txtBraille_TextChanged);
+            this.txtBraille.Validating += new System.ComponentModel.CancelEventHandler(this.txtBraille_Validating);
             // 
             // btnOk
             // 
@@ -127,7 +127,7 @@
             // btnPickBraille
             // 
             this.btnPickBraille.Image = global::EasyBrailleEdit.Properties.Resources.PickBraille;
-            this.btnPickBraille.Location = new System.Drawing.Point(192, 116);
+            this.btnPickBraille.Location = new System.Drawing.Point(240, 119);
             this.btnPickBraille.Name = "btnPickBraille";
             this.btnPickBraille.Size = new System.Drawing.Size(32, 36);
             this.btnPickBraille.TabIndex = 8;
@@ -140,7 +140,7 @@
             // 
             // EditCellForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(303, 239);
             this.Controls.Add(this.btnPickBraille);
@@ -162,8 +162,8 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "修改點字";
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.EditCellForm_KeyUp);
             this.Load += new System.EventHandler(this.EditCellForm_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.EditCellForm_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
