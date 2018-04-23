1eaea3cb510bea40ff284c7fbaf717c50c68dc08
Michael Tsai <huanlin.tsai@gmail.com>
2018-04-24 02:10:55 +0800

    Added more tests.

---
+++ Source/BrailleToolkit/Helpers/BrailleWordHelper.cs
@@ -0,0 +1,30 @@
+﻿using Huanlin.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrailleToolkit.Extensions
+{
+    public static class BrailleWordHelper
+    {
+        public static string ToString(this List<BrailleWord> brWordList)
+        {
+            var sb = new StringBuilder();
+            foreach (var brWord in brWordList)
+            {            
+                sb.Append("{");
+                sb.Append($"\"{brWord.Text}\",");
+                sb.Append($"\"{brWord.PhoneticCode}\",");
+                sb.Append($"\"{brWord.CellList.ToString()}\"");
+                sb.Append("},");
+            }
+            if (sb.Length > 0)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+            return sb.ToString();
+        }
+    }
+}
